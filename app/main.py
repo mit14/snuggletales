12006a843587f1f0897ae48b1bf84540d514b2ec
mit14 <mit14@live.com>
2024-05-21 21:48:58 -0400

    adding middleware

--- app/main.py
+++ app/main.py
@@ -4,11 +4,25 @@ from .database import Base, engine
 from app.routers import auth
 from .config import settings
 
+from fastapi.middleware.cors import CORSMiddleware
+
 
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 # app.include_router(registration.router)
 app.include_router(auth.router)
