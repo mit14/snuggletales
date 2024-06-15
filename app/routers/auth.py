fde6e8c295c9f6a38088b6d4d2513061c8ed8226
mit14 <mit14@live.com>
2024-06-15 00:04:06 -0400

    added login via google

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,7 +1,9 @@
 from fastapi import  Request, status, HTTPException, Depends, APIRouter
 from fastapi import BackgroundTasks
 from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2
 from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from authlib.integrations.starlette_client import OAuth
 from slowapi import Limiter
 from slowapi.errors import RateLimitExceeded
 from slowapi.util import get_remote_address
@@ -12,6 +14,8 @@ from app import database, utils, schemas, models, oauth2
 from app.config import settings
 limiter = Limiter(key_func=get_remote_address)
 
+#import required for login via google
+oauth = OAuth()
 
 router = APIRouter(prefix= "/api/dev/v1/user",
                    tags= ["Registration and Authentication"]
@@ -170,4 +174,39 @@ def login_user(request: Request, user_credentials: OAuth2PasswordRequestForm = D
     
     access_token = oauth2.create_access_token(data={"user_id":user.id})
     logger.info(f"Successful login for user: {user_credentials.username}")
+    return {"access_token": access_token, "token_type": "bearer"}
+
+################################################### LOGIN WITH GOOGLE  ################################################################################
+
+google = oauth.register(
+    name='google',
+    client_id=settings.google_client_id,
+    client_secret=settings.google_client_secret,
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    api_base_url='https://www.googleapis.com/oauth2/v1/',
+    client_kwargs={'scope': 'openid email profile'}
+)
+
+@router.get('/login/google')
+async def login_via_google(request: Request):
+    redirect_uri = request.url_for('authorize_google')
+    return await google.authorize_redirect(request, redirect_uri)
+
+@router.get('/authorize/google')
+async def authorize_google(request: Request, db: Session = Depends(database.get_db)):
+    token = await google.authorize_access_token(request)
+    user_info = await google.parse_id_token(request, token)
+    email = user_info.get('email')
+    provider_id = user_info.get('sub')
+
+    user = db.query(models.User).filter_by(email=email, provider='google').first()
+    if not user:
+        user = models.User(email=email,
+                           provider='google', 
+                           provider_id=provider_id, is_verified=True)
+        db.add(user)
+        db.commit()
+
+    access_token = oauth2.create_access_token(data={"user_id": user.id})
     return {"access_token": access_token, "token_type": "bearer"}
