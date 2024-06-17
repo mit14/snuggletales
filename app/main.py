8109e2cc4714836a102fb44feacf8f5030255f35
mit14 <mit14@live.com>
2024-06-16 22:32:12 -0400

    Revert "added middleware for session"
    This reverts commit 1cd9039f306c34e526c79b08d48ec6f2b58f0409.

--- app/main.py
+++ app/main.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from starlette.middleware.sessions import SessionMiddleware  # Import the SessionMiddleware
 
 from .utils import limiter, setup_exception_handlers, setup_logger
 from .database import Base, engine
 from app.routers import auth, admin_portal, stories, like_story, update_profile
 from .config import settings
 
+
+
+
+
+# Base.metadata.create_all(bind=engine) ##// deactivated because using alembic 
+
 app = FastAPI()
 
-
-app.add_middleware(SessionMiddleware, secret_key=settings.secret_key)
-
-# Configure CORS
 origins = ["*"]
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -22,19 +24,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Application state and setup
+
 app.state.limiter = limiter
 setup_exception_handlers(app)
+
 logger = setup_logger()
 
-# Include routers from various modules
+
+# app.include_router(registration.router)
 app.include_router(auth.router)
 app.include_router(admin_portal.router)
 app.include_router(stories.router)
 app.include_router(like_story.router)
 app.include_router(update_profile.router)
 
+
 @app.get("/")
 def root():
     logger.info("Root endpoint was accessed")
     return {"message": "Hello World"}
+
